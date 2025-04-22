3d8dbef689b17b88b3ffbba18b50e2a20cc519ca
Xinlei Wen <xinlei.wen@anu.edu.au>
2025-04-22 17:54:17 +1000

    Fix the wierd cache bug

--- objloader.h
+++ objloader.h
@@ -1,3 +1,6 @@
+#ifndef OBJ_LOADER_H
+#define OBJ_LOADER_H
+
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -7,8 +10,6 @@
 #include "material.h"
 
 using namespace Eigen;
-
-std::vector<std::string> split(const std::string &s, char delimiter);
 
 class Face{
     public:
@@ -25,12 +26,23 @@ class ObjLoader{
     public:
         std::vector<Vector3f> vertices; // vertices
         std::vector<Vector3f> normals;  // normals
-        std::vector<Vector3f> tex_coords;   // texture coordinates
-        std::vector<Face> faces;    // triangles
-        std::vector<triangle> triangles;
+        std::vector<Vector2f> tex_coords;   // texture coordinates
+        std::vector<shared_ptr<Face>> faces;    // triangles
+        std::vector<shared_ptr<triangle>> triangles;
 
         ObjLoader() {}
         ~ObjLoader() {}
+        
+        // this function is defined for spliting three coordinates of a vertex in a face
+        std::vector<std::string> split(const std::string &s, char delimiter) {
+            std::vector<std::string> tokens;
+            std::string token;
+            std::istringstream tokenStream(s);
+            while (std::getline(tokenStream, token, delimiter)) {
+                if (!token.empty()) tokens.push_back(token);
+            }
+            return tokens;
+        };
 
         inline bool read_obj(const std::string& filepath, const std::string& texturepath){
             if(filepath.substr(filepath.size()-4, 4) != ".obj"){
@@ -55,48 +67,43 @@ class ObjLoader{
                     Vector3f vertex;
                     iss >> vertex[0] >> vertex[1] >> vertex[2];
                     this->vertices.push_back(vertex);
-                } else if ("vn" == prefix) {
+                 } else if ("vn" == prefix) {
                     Vector3f normal;
                     iss >> normal[0] >> normal[1] >> normal[2];
                     this->normals.push_back(normal);
                 } else if ("vt" == prefix) {
-                    Vector3f texcoord;
-                    iss >> texcoord[0] >> texcoord[1] >> texcoord[2];
+                    Vector2f texcoord;
+                    iss >> texcoord[0] >> texcoord[1];
                     this->tex_coords.push_back(texcoord);
                 } 
                 else if ("f" == prefix) {
-                    Face face;
+                    auto face = make_shared<Face>();
                     std::string vert[3];
                     iss >> vert[0] >> vert[1] >> vert[2];
                     for(int i = 0; i < 3; i++){
                         auto elements = split(vert[i], '/');
-                        face.vert_pos[i] = std::stoi(elements[0]);
-                        face.tex_coords[i] = std::stoi(elements[1]);
-                        face.normals[i] = std::stoi(elements[2]);
+                        face->vert_pos[i] = std::stoi(elements[0]);
+                        face->tex_coords[i] = std::stoi(elements[1]);
+                        face->normals[i] = std::stoi(elements[2]);
                     }
                     // create triangle
                     auto mat = std::make_shared<textured_lambertian>(texture_data);
-                    triangle(this->vertices[face.vert_pos[0]],
-                        this->vertices[face.vert_pos[1]],
-                        this->vertices[face.vert_pos[2]],   // vertices
-                        this->tex_coords[face.tex_coords[0]],
-                        this->tex_coords[face.tex_coords[1]],
-                        this->tex_coords[face.tex_coords[2]],
+                
+                    auto tri = make_shared<triangle>(
+                        this->vertices[face->vert_pos[0]],
+                        this->vertices[face->vert_pos[1]],
+                        this->vertices[face->vert_pos[2]],   // vertices
+                        this->tex_coords[face->tex_coords[0]],
+                        this->tex_coords[face->tex_coords[1]],
+                        this->tex_coords[face->tex_coords[2]],
                         mat
                     );
-                    this->faces.push_back(face);
+                    // this->faces.push_back(face);
+                    this->triangles.push_back(tri);
                 }
             }
+            return true;
         }
 };
 
-// this function is defined for spliting three coordinates of a vertex in a face
-inline std::vector<std::string> split(const std::string &s, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(s);
-    while (std::getline(tokenStream, token, delimiter)) {
-        if (!token.empty()) tokens.push_back(token);
-    }
-    return tokens;
-}
+#endif
